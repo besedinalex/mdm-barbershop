cb1dba9eaf5c19f7e16a44bdc45e7fb9a49f2d23
Alexey Besedin <besedinalexey@gmail.com>
2020-12-24 23:31:08 +0300

    View and add users

--- BarbershopMDM/Forms/AdminForm.Designer.cs
+++ BarbershopMDM/Forms/AdminForm.Designer.cs
@@ -29,20 +29,163 @@ namespace BarbershopMDM.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridViewUsers = new System.Windows.Forms.DataGridView();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxRole = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // dataGridViewUsers
+            // 
+            this.dataGridViewUsers.AllowUserToAddRows = false;
+            this.dataGridViewUsers.AllowUserToDeleteRows = false;
+            this.dataGridViewUsers.AllowUserToResizeColumns = false;
+            this.dataGridViewUsers.AllowUserToResizeRows = false;
+            this.dataGridViewUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewUsers.Location = new System.Drawing.Point(12, 12);
+            this.dataGridViewUsers.Name = "dataGridViewUsers";
+            this.dataGridViewUsers.ReadOnly = true;
+            this.dataGridViewUsers.Size = new System.Drawing.Size(534, 89);
+            this.dataGridViewUsers.TabIndex = 0;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(77, 19);
+            this.textBoxName.MaxLength = 200;
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(451, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(77, 45);
+            this.textBoxLogin.MaxLength = 20;
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(451, 20);
+            this.textBoxLogin.TabIndex = 2;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(414, 124);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(114, 23);
+            this.buttonAdd.TabIndex = 3;
+            this.buttonAdd.Text = "Добавить";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxRole);
+            this.groupBox1.Controls.Add(this.buttonAdd);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxPassword);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxName);
+            this.groupBox1.Controls.Add(this.textBoxLogin);
+            this.groupBox1.Location = new System.Drawing.Point(12, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(534, 153);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ФИО";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Логин";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(77, 71);
+            this.textBoxPassword.MaxLength = 56;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(451, 20);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Пароль";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Должность";
+            // 
+            // comboBoxRole
+            // 
+            this.comboBoxRole.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxRole.FormattingEnabled = true;
+            this.comboBoxRole.Location = new System.Drawing.Point(77, 97);
+            this.comboBoxRole.Name = "comboBoxRole";
+            this.comboBoxRole.Size = new System.Drawing.Size(451, 21);
+            this.comboBoxRole.TabIndex = 7;
             // 
             // AdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(559, 272);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridViewUsers);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "AdminForm";
-            this.Text = "AdminForm";
+            this.Text = "Управление пользователями";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AdminForm_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewUsers)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewUsers;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.ComboBox comboBoxRole;
     }
 }
--- BarbershopMDM/Forms/AdminForm.cs
+++ BarbershopMDM/Forms/AdminForm.cs
@@ -7,19 +7,94 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BarbershopMDM.Data.Repositories;
 
 namespace BarbershopMDM.Forms
 {
     public partial class AdminForm : Form
     {
+        private readonly IEmployeesRepository _employeesRepository;
+
         public AdminForm()
         {
+            _employeesRepository = (IEmployeesRepository)Program.ServiceProvider.GetService(typeof(IEmployeesRepository));
+
             InitializeComponent();
+
+            dataGridViewUsers.RowHeadersVisible = false;
+            dataGridViewUsers.ColumnCount = 3;
+            dataGridViewUsers.Columns[0].HeaderText = "ФИО";
+            dataGridViewUsers.Columns[1].HeaderText = "Логин";
+            dataGridViewUsers.Columns[2].HeaderText = "Должность";
+            dataGridViewUsers.Columns[1].Width = 110;
+            dataGridViewUsers.Columns[2].Width = 90;
+
+            UpdateDataGridView();
+
+            comboBoxRole.Items.AddRange(new string[] { "Бухгалтер", "Администратор" });
+            comboBoxRole.SelectedIndex = 0;
+        }
+
+        private async void UpdateDataGridView()
+        {
+            dataGridViewUsers.Rows.Clear();
+            var users = await _employeesRepository.GetEmployees();
+            dataGridViewUsers.Columns[0].Width = users.Count > 3 ? 313 : 330;
+            foreach (var user in users)
+            {
+                var role = user.Role.Equals("accountant") ? "Бухгалтер" : user.Role.Equals("manager") ? "Администратор" : "Неизвестно";
+                dataGridViewUsers.Rows.Add(new[] { user.Name, user.Login, role });
+            }
         }
 
         private void AdminForm_FormClosed(object sender, FormClosedEventArgs e)
         {
             Program.LoginForm.Show();
+
+        private async void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var name = textBoxName.Text;
+            var login = textBoxLogin.Text;
+            var password = textBoxPassword.Text;
+
+            var message = "";
+
+            if (name.Length < 3)
+            {
+                message += "Поле \"ФИО\" должно содержать минимум 3 символа.\n";
+            }
+
+            if (login.Length < 3)
+            {
+                message += "Поле \"Логин\" должно содержать минимум 3 символа.\n";
+            }
+
+            if (password.Length < 8)
+            {
+                message += "Поле \"Пароль\" должно содержать минимум 8 символов.\n";
+            }
+
+            if (message != "")
+            {
+                MessageBox.Show(message, "Валидация данных пользователя");
+                return;
+            }
+
+            var user = new Models.Employee()
+            {
+                Name = name,
+                Login = login,
+                Password = password,
+                Role = comboBoxRole.SelectedIndex == 0 ? "accountant" : "manager"
+            };
+
+            await _employeesRepository.CreateEmployee(user);
+            
+            UpdateDataGridView();
+
+            textBoxName.Text = "";
+            textBoxLogin.Text = "";
+            textBoxPassword.Text = "";
         }
     }
 }
